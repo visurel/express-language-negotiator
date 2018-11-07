3632cc748551f300473a14f1e81dcf37865fa4e7
visurel <martin@visurel.com>
2018-11-07 16:47:37 +0100

    chore: add tests and fix issues

--- src/lib/language-negotiator.ts
+++ src/lib/language-negotiator.ts
@@ -1,11 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
 
 export interface LanguageNegotiatorOptions {
-  priority: ReadonlyArray<'cookie' | 'query' | 'acceptLanguage'>
-  languages: ReadonlyArray<string>
-  defaultLanguage: string
-  cookieName?: string
-  queryParamName?: string
+  priority: Array<'cookie' | 'query' | 'acceptLanguage'>
+  languages: string[];
+  defaultLanguage: string;
+  cookieName?: string;
+  queryParamName?: string;
 }
 
 declare global {
@@ -16,7 +16,7 @@ declare global {
   }
 }
 
-export function languageNegotiator(options: LanguageNegotiatorOptions): (req: Request, res: Response, next: NextFunction) => void {
+export function negotiateLanguage(options: LanguageNegotiatorOptions): (req: Request, res: Response, next: NextFunction) => void {
   if (options.languages.length === 0) {
     throw new Error('Please provide at least 1 supported language');
   }
@@ -30,12 +30,12 @@ export function languageNegotiator(options: LanguageNegotiatorOptions): (req: Re
       return options.languages.indexOf(lang) > -1;
     }
 
-    for (const method in options.priority) {
+    for (const method of options.priority) {
       switch (method) {
         case 'cookie': {
           if (req.cookies) {
             const cookie = req.cookies[options.cookieName || 'language'];
-            if (contains(cookie)) {
+            if (cookie && contains(cookie)) {
               match = cookie;
             }
           }
@@ -45,7 +45,7 @@ export function languageNegotiator(options: LanguageNegotiatorOptions): (req: Re
         case 'query': {
           if (req.query) {
             const query = req.query[options.queryParamName || 'language'];
-            if (contains(query)) {
+            if (query && contains(query)) {
               match = query;
             }
           }
@@ -56,13 +56,15 @@ export function languageNegotiator(options: LanguageNegotiatorOptions): (req: Re
           if (req.headers['accept-language']) {
             const acceptedLanguages = req.acceptsLanguages();
 
-            const directMatch = acceptedLanguages.find(language => options.languages.findIndex(lang => lang === language) > -1);
+            const directMatch = acceptedLanguages.find(lang => options.languages.findIndex(l => l === lang) > -1);
             if (directMatch) {
               match = directMatch;
               break;
             }
 
-            const indirectMatch = acceptedLanguages.find(language => options.languages.findIndex(lang => lang.substring(0, 2) === language.substring(0, 2)) > -1);
+            const acceptedLanguagesIndirect = acceptedLanguages.map(lang => lang.substring(0, 2));
+            const languages = options.languages.map(lang => lang.substring(0, 2));
+            const indirectMatch = acceptedLanguagesIndirect.find(lang => languages.findIndex(l => lang === l) > -1);
 
             if (indirectMatch) {
               match = indirectMatch;
