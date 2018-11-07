69568e2a20bcca2a1bfa84490d51b00806d658bb
visurel <martin@visurel.com>
2018-11-07 16:17:37 +0100

    feat: initial version

---
+++ src/lib/language-negotiator.ts
@@ -0,0 +1,88 @@
+import { NextFunction, Request, Response } from 'express';
+
+export interface LanguageNegotiatorOptions {
+  priority: ReadonlyArray<'cookie' | 'query' | 'acceptLanguage'>
+  languages: ReadonlyArray<string>
+  defaultLanguage: string
+  cookieName?: string
+  queryParamName?: string
+}
+
+declare global {
+  namespace Express {
+    export interface Request {
+      language?: string
+    }
+  }
+}
+
+export function languageNegotiator(options: LanguageNegotiatorOptions): (req: Request, res: Response, next: NextFunction) => void {
+  if (options.languages.length === 0) {
+    throw new Error('Please provide at least 1 supported language');
+  }
+
+  // @ts-ignore
+  return (req: Request, res: Response, next: NextFunction) => {
+    let language = options.defaultLanguage;
+    let match: string | null = null;
+
+    function contains(lang: string): boolean {
+      return options.languages.indexOf(lang) > -1;
+    }
+
+    for (const method in options.priority) {
+      switch (method) {
+        case 'cookie': {
+          if (req.cookies) {
+            const cookie = req.cookies[options.cookieName || 'language'];
+            if (contains(cookie)) {
+              match = cookie;
+            }
+          }
+          break;
+        }
+
+        case 'query': {
+          if (req.query) {
+            const query = req.query[options.queryParamName || 'language'];
+            if (contains(query)) {
+              match = query;
+            }
+          }
+          break;
+        }
+
+        case 'acceptLanguage': {
+          if (req.headers['accept-language']) {
+            const acceptedLanguages = req.acceptsLanguages();
+
+            const directMatch = acceptedLanguages.find(language => options.languages.findIndex(lang => lang === language) > -1);
+            if (directMatch) {
+              match = directMatch;
+              break;
+            }
+
+            const indirectMatch = acceptedLanguages.find(language => options.languages.findIndex(lang => lang.substring(0, 2) === language.substring(0, 2)) > -1);
+
+            if (indirectMatch) {
+              match = indirectMatch;
+              break;
+            }
+          }
+          break;
+        }
+      }
+
+      if (match) {
+        break;
+      }
+    }
+
+    if (match) {
+      language = match;
+    }
+
+    req.language = language;
+    next();
+  };
+}
